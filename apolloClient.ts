8fdce085fab02a21b97070e70fdb7fb2765b60ae
adalbertopc <adalbertinho90@gmail.com>
2021-12-04 23:22:02 -0700

    ref: change apollo client

--- apolloClient.ts
+++ apolloClient.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const apolloClient = new ApolloClient({
-  uri: `http://localhost:8080/graphql`,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
