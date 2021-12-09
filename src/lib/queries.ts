135e63f6e1bf93e08360762200720a124af7a450
adalbertopc <adalbertinho90@gmail.com>
2021-12-09 15:05:02 -0700

    fix: fix queries

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,24 +1,20 @@
 import { gql } from '@apollo/client';
 
-export const GET_POOL = (id: string) => gql`
-query {
-  pool(id: "${id}") {
-    _id
-    name
-    description
-    location
-    specs{
-      width
-      length
-      depth
-      capacity
-    }
-    sensorDataHistory {
-      date
-      ph
+export const GET_POOL = gql`
+  query Pool($id: ID!) {
+    pool(id: $id) {
+      _id
+      name
+      description
+      location
+      volumen
+      sensorDataHistory {
+        date
+        ph
+      }
     }
   }
-}`;
+`;
 
 export const GET_USER_POOLS = (id: string) => gql`
 query {
@@ -32,3 +28,30 @@ query {
     }
   }
 }`;
+
+export const CREATE_POOL = gql`
+  mutation CreatePool(
+    $owner: ID!
+    $name: String!
+    $description: String
+    $location: String
+    $volumen: Int!
+    $image: String
+    $sensorID: String!
+  ) {
+    createPool(
+      input: {
+        name: $name
+        description: $description
+        location: $location
+        image: $image
+        owner: $owner
+        volumen: $volumen
+        sensorID: $sensorID
+      }
+    ) {
+      name
+      _id
+    }
+  }
+`;
